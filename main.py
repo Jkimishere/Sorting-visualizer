eac6ff8e1c4cfec2114e7be6b1913a712e350d4f
Jkimishere <106307588+Jkimishere@users.noreply.github.com>
2023-12-31 15:00:30 +0900

    Finished quicksort with better colors, made code cleaner

--- main.py
+++ main.py
@@ -1,72 +1,84 @@
+#Importing libraries
+
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button,Slider 
+from matplotlib.widgets import Button, Slider
 import random
 from sorter import quicksort
-import threading
-import psutil
 
+#Modify if needed
 DELAY_TIME = 0.1
+BASE_COLOR = ['#5b92c9']
+COLORS_BASE = BASE_COLOR * 100
+
+# Function to plot the list
+def plot_values(values, title='Visualizing sorting algorithms', colors=None):
+    """
+    Function to plot values on the plot.
+
+    Parameters:
+    - values (list) : List of the values to be plotted
+    - title (str) : Title of the plot
+    - colors (list) : The colors of the bars
+
+    Raises:
+    - ValueError : If length of values and colors are different.
+    """
 
 
-def plot_values(values:list, title:str = 'Visualizing sorting algorithms', ):
-    global DELAY_TIME
-    print('starting plot')
+    c = colors if colors is not None else COLORS_BASE
+    if len(values) != len(c):
+        raise ValueError("Length of 'values' and 'colors' must be the same.")
     ax.clear()
-    print('cleared')
-    ax.bar(range(len(values)), values)
-    ax.set_title(title) 
+    ax.bar(range(len(values)), values, color=c)
+    ax.set_title(title)
     plt.pause(DELAY_TIME)
-    plt.ioff()
     plt.show(block=True)
 
-
-
 def shuffle_values(event):
+    """
+    Shuffles the values and plots them.
+    """
     global values
     random.shuffle(values)
     plot_values(values, 'Visualizing sorting algorithms')
 
+
+# Event handler to perform quicksort on values
 def quicksort_values(event):
-    global quicksort_button
-    global shuffle_button
-    global slider
-    global values
-    quicksort_button.active=False
-    shuffle_button.active=False
-    slider.active = False
-    quicksort(values,0,len(values)-1, plot_values, fig.number)
+    global quicksort_button, shuffle_button, slider, values
+    quicksort_button.active = shuffle_button.active = slider.active = False
+    quicksort(values, 0, len(values) - 1, plot_values, fig.number)
     plot_values(values, 'Done quicksorting')
-    quicksort_button.active=True
-    shuffle_button.active=True
-    slider.active = True
+    quicksort_button.active = shuffle_button.active = slider.active = True
 
+# Event handler to change length of the list
 def change_length_values(val):
-    global values
-    print(val)
-    values = list(range(val))
+    global values, BASE_COLOR, COLORS_BASE
+    values = list(range(val + 1))
+    COLORS_BASE = BASE_COLOR * (val + 1)
     plot_values(values)
-values = list(range(1, 100))
 
+
+# Initial values for the array
+values = list(range(1, 51))
+
+# Create plot
 fig, ax = plt.subplots()
-bar_plot = ax.bar(range(len(values)), values)
+ax.bar(range(len(values)), values, color=COLORS_BASE)
 ax.set_title('Visualizing sorting algorithms')
 
 
-shuffle_button_ax = plt.axes([0.85, 0.01, 0.1, 0.05])  # [x, y, width, height]
+# UI code
+shuffle_button_ax = plt.axes([0.85, 0.01, 0.1, 0.05])
 shuffle_button = Button(shuffle_button_ax, 'Shuffle')
 shuffle_button.on_clicked(shuffle_values)
 
-quicksort_button_ax = plt.axes([0.72, 0.01, 0.1, 0.05])  # [x, y, width, height]
+quicksort_button_ax = plt.axes([0.72, 0.01, 0.1, 0.05])
 quicksort_button = Button(quicksort_button_ax, 'Quicksort')
 quicksort_button.on_clicked(quicksort_values)
 
-slider = Slider(plt.axes([0.1, 0.01, 0.4, 0.03]), 'Length of array:', 10, 100, valinit=100, valstep=1)
+slider = Slider(plt.axes([0.1, 0.01, 0.4, 0.03]), 'Length of array:', 10, 100, valinit=50, valstep=1)
 slider.on_changed(change_length_values)
 
-
-# Display the plot
-
-
+# Show the plot
 plt.show(block=True)
-
-
--- sorter.py
+++ sorter.py
@@ -2,26 +2,36 @@ import random
 import time
 import matplotlib.pyplot as plt
 
+
+BASE_COLOR = ['#5b92c9']
+
 def quicksort_partition(arr, start, end, plot_func, fignum):
+    
     pivot = arr[end]
     i = start - 1
     temp = 0
     for j in range(start, end):
+        c = BASE_COLOR * len(arr)
         if not plt.fignum_exists(fignum):
             return
-        #time.sleep(0.03)
         if arr[j] <= pivot:
             i +=1
+            c[i] = '#d95555'
+            c[j] = '#d95555'
             temp = arr[i]
             arr[i] = arr[j]
             arr[j] = temp
-            plot_func(arr, 'quicksorting....')
-    #time.sleep(0.03)
+            c[end] = '#55d971'
+
+            plot_func(arr, 'quicksorting....',c)
+    c= BASE_COLOR * len(arr)
     i +=1
     temp = arr[i]
     arr[i] = arr[end]
     arr[end] = temp
-    plot_func(arr, 'quicksorting....')
+    c[end] = '#55d971'
+    c[i] = '#d95555'
+    plot_func(arr, 'quicksorting....',c)
     return i
             
 
@@ -47,7 +57,7 @@ def test_quicksort(length_arr):
     test_arr = list(range(1,1000))
     random.shuffle(test_arr)
 
-    plsman = quicksort(test_arr, 0, len(test_arr)-1)
-    print(plsman == sorted(plsman))
+    sorted_arr = quicksort(test_arr, 0, len(test_arr)-1)
+    print(sorted_arr == sorted(test_arr))
 
     
