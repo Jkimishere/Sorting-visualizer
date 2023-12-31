d16c57cc5a95a0c4b17825437251288a8845151d
Jkimishere <106307588+Jkimishere@users.noreply.github.com>
2023-12-31 10:46:59 +0900

    finished quicksort visualization with barebones gui

--- main.py
+++ main.py
@@ -1,31 +1,72 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
+from matplotlib.widgets import Button,Slider 
 import random
 from sorter import quicksort
+import threading
+import psutil
+
+DELAY_TIME = 0.1
+
+
+def plot_values(values:list, title:str = 'Visualizing sorting algorithms', ):
+    global DELAY_TIME
+    print('starting plot')
+    ax.clear()
+    print('cleared')
+    ax.bar(range(len(values)), values)
+    ax.set_title(title) 
+    plt.pause(DELAY_TIME)
+    plt.ioff()
+    plt.show(block=True)
+
+
 
 def shuffle_values(event):
     global values
     random.shuffle(values)
-    ax.clear()
-    ax.bar(range(len(values)), values)
-    ax.set_xlabel('Index')
-    ax.set_ylabel('Values')
-    ax.set_title('Shuffled Bar Chart')
-    plt.draw()
+    plot_values(values, 'Visualizing sorting algorithms')
 
+def quicksort_values(event):
+    global quicksort_button
+    global shuffle_button
+    global slider
+    global values
+    quicksort_button.active=False
+    shuffle_button.active=False
+    slider.active = False
+    quicksort(values,0,len(values)-1, plot_values, fig.number)
+    plot_values(values, 'Done quicksorting')
+    quicksort_button.active=True
+    shuffle_button.active=True
+    slider.active = True
 
+def change_length_values(val):
+    global values
+    print(val)
+    values = list(range(val))
+    plot_values(values)
 values = list(range(1, 100))
 
 fig, ax = plt.subplots()
 bar_plot = ax.bar(range(len(values)), values)
-ax.set_xlabel('Index')
-ax.set_ylabel('Values')
-ax.set_title('Bar Chart with Shuffle Button')
+ax.set_title('Visualizing sorting algorithms')
 
-# Adding the shuffle button
+
 shuffle_button_ax = plt.axes([0.85, 0.01, 0.1, 0.05])  # [x, y, width, height]
 shuffle_button = Button(shuffle_button_ax, 'Shuffle')
 shuffle_button.on_clicked(shuffle_values)
 
+quicksort_button_ax = plt.axes([0.72, 0.01, 0.1, 0.05])  # [x, y, width, height]
+quicksort_button = Button(quicksort_button_ax, 'Quicksort')
+quicksort_button.on_clicked(quicksort_values)
+
+slider = Slider(plt.axes([0.1, 0.01, 0.4, 0.03]), 'Length of array:', 10, 100, valinit=100, valstep=1)
+slider.on_changed(change_length_values)
+
+
 # Display the plot
-plt.show()
+
+
+plt.show(block=True)
+
+
--- sorter.py
+++ sorter.py
@@ -1,28 +1,43 @@
 import random
+import time
+import matplotlib.pyplot as plt
 
-def quicksort_partition(arr, start, end):
+def quicksort_partition(arr, start, end, plot_func, fignum):
     pivot = arr[end]
     i = start - 1
     temp = 0
     for j in range(start, end):
+        if not plt.fignum_exists(fignum):
+            return
+        #time.sleep(0.03)
         if arr[j] <= pivot:
             i +=1
             temp = arr[i]
             arr[i] = arr[j]
             arr[j] = temp
+            plot_func(arr, 'quicksorting....')
+    #time.sleep(0.03)
     i +=1
     temp = arr[i]
     arr[i] = arr[end]
     arr[end] = temp
+    plot_func(arr, 'quicksorting....')
     return i
             
 
 
-def quicksort(arr, start, end, plot_func=None):
+def quicksort(arr, start, end, plot_func, fignum):
+    if not plt.fignum_exists(fignum):
+        return
     if start < end: 
-        pivot = quicksort_partition(arr, start, end)
-        quicksort(arr, start, pivot - 1)
-        quicksort(arr, pivot + 1, end)
+        pivot = quicksort_partition(arr, start, end, plot_func, fignum)
+        #time.sleep(0.03)
+        try:
+            quicksort(arr, start, pivot - 1, plot_func, fignum)
+            quicksort(arr, pivot + 1, end, plot_func, fignum)
+        except TypeError:
+            print("\033[1;31;40m You closed the plot. \033[m")
+
     return arr
 
 
